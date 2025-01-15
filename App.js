b6f2406455517c936f47b17943fab3738cb600de
Prachi Kodle <prachikodle@gmail.com>
2025-01-15 17:48:07 +0530

    feat: initial commit for react app created from scratch

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const heading = <h1>Food Ordering App</h1>;
+
+const root = createRoot(document.getElementById("root"));
+root.render(heading);
